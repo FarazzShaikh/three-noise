7ac8896a26ba12950609bd18308dc573cff90fac
Faraz Shaikh <farazzshaikh@gmail.com>
2021-05-02 16:16:52 +0530

    updated docs generation script

--- scripts/doc.sh
+++ scripts/doc.sh
@@ -5,6 +5,22 @@ replace="<h2><a href=\"index.html\">Home<\/a><\/h2><h3><a href=\".\/\example\/in
 
 
 lineNum=$(grep -n "<h2><a href=\"index.html\">Home</a></h2>" docs/index.html | cut -d: -f1)
-sed -i '' "${lineNum}s/$match/$replace/" docs/index.html
+
+
+
+unameOut="$(uname -s)"
+case "${unameOut}" in
+    Linux*)     machine=Linux;;
+    Darwin*)    machine=Mac;;
+    CYGWIN*)    machine=Cygwin;;
+    MINGW*)     machine=MinGw;;
+    *)          machine="UNKNOWN:${unameOut}"
+esac
+
+if [ "$machine" = "Mac" ]; then
+    sed -i '' "${lineNum}s/$match/$replace/" docs/index.html
+else
+    sed -i "${lineNum}s/$match/$replace/" docs/index.html
+fi
 
 cp -R example docs/example
